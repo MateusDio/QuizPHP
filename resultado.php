71dd1212b6ecb269f661dfffca59aaa7b9f121df
MateusDio <165714347+MateusDio@users.noreply.github.com>
2026-03-11 16:57:34 -0300

    Add files via upload

--- resultado.php
+++ resultado.php
@@ -32,25 +32,30 @@ if($r=="robotica") $robotica++;
 if($design >= $logica && $design >= $games && $design >= $robotica){
 $perfil="Mestre do Design";
 $img = "img/designer.png";
+$descricao = "Você tem uma mente criativa e gosta de transformar ideias em coisas visuais. Design, interfaces e estética são áreas onde você pode brilhar.";
 }
 
 elseif($logica >= $design && $logica >= $games && $logica >= $robotica){
 $perfil="Hacker da Lógica";
 $img = "img/hacker.png";
+$descricao = "Você gosta de resolver problemas e pensar de forma estratégica. Programação, algoritmos e desafios de lógica combinam muito com você.";
 }
+
 elseif($games >= $design && $games >= $logica && $games >= $robotica){
 $perfil="Criador de Games";
 $img = "img/jogo.png";
+$descricao = "Você tem imaginação e gosta de criar experiências divertidas. Desenvolver jogos e mundos interativos pode ser seu caminho.";
 }
 
 elseif($robotica >= $design && $robotica >= $logica && $robotica >= $games){
 $perfil="Engenheiro de Robótica";
 $img = "img/robotica.png";
-
+$descricao = "Você gosta de tecnologia prática e de construir coisas que funcionam no mundo real. Robôs, eletrônica e automação são sua praia.";
 }
 
 else{
 $perfil="Explorador da Tecnologia";
+$descricao = "Você tem interesse em várias áreas da tecnologia e gosta de experimentar coisas novas.";
 }
 
 setcookie("jogador",$nome,time()+3600);
@@ -73,11 +78,12 @@ setcookie("jogador",$nome,time()+3600);
 <p>Jogador: <?php echo $nome; ?></p>
 
 <h2>Seu perfil é: <?php echo $perfil; ?></h2>
+
 <img src="<?php echo $img; ?>" alt="Resultado">
 
+<p class="descricao"><?php echo $descricao; ?></p>
 
 <a href="index.php" class="btn">Jogar novamente</a>
-</a>
 </div>
 </body>
 </html>
